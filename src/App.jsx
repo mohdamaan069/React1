f8bd057b6ad2bed88e970fd4a86a763839132fc2
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2023-09-28 01:17:47 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <button className="bg-green-400 text-white rounded-xl p-4 mb-4">Props</button>
+    
+  </>
+  )
+}
+
+export default App;
