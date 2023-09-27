d6b05428f23edec8ce4f6b8bdc55de6d44cbe0fd
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2023-09-28 03:08:00 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react"
+import { GiCrossedSwords } from "react-icons/gi";
 
 
 function App() {
   const [color, setColor] = useState("olive")    // State Hook
 
   return (
+    <>
+    
     <div className="w-full h-screen duration-200"
     style={{backgroundColor: color}}
-    >
+      >
+      <div className="fixed  justify-center bottom-100 inset-x-0 text-center text-2xl m-6 text-white font-mono italic px-2">Background Color Changer</div>
+        <div className="fixed ml-96   bottom-96   px-2 "><GiCrossedSwords size="150" color="white" style={{ alignItems: "center", display: "flex" }} /></div>
+        <div className="fixed right-96   bottom-96   px-2 "><GiCrossedSwords size="150" color = "white" style={{alignItems:"center", display: "flex"}}  /></div>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
@@ -52,7 +58,8 @@ function App() {
           >Aqua</button>
         </div>
       </div>
-    </div>
+      </div>
+      </>
   )
 }
 
